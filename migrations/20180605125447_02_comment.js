7ab20b75c8e4a6df25efb72698a56f7a4af31fd4
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-06-05 16:07:48 -0600

    add user info

--- migrations/20180605125447_02_comment.js
+++ migrations/20180605125447_02_comment.js
@@ -2,7 +2,8 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('comment', (table) => {
       table.increments('comment_id');
       table.integer('comment_store_id').references('store.store_id').unsigned();
-      table.text('user_name').notNullable();
+      table.text('user_name');
+      table.specificType('user_pic', 'TEXT[]')
       table.text('comment');
       table.integer('rating');
     });
