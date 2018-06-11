84551b11f9230396a58b84c7f45c9fa287ce6bb3
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-06-11 13:22:15 -0600

    add user pic to seed

--- seed_data_files/comments-seed-data.js
+++ seed_data_files/comments-seed-data.js
@@ -2,35 +2,35 @@ module.exports = [
     { 
         "comment_store_id": 1,
         "user_name": "Michael Smith",
-        "user_pic": {},
+        "user_pic": "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=10155410113061073&height=200&width=200&ext=1529003986&hash=AeQa7dc2YcMAFiC4",
         "comment": "Great store! They have a lot to choose from. Store is clean and staff is very helpful.",
         "rating": 5
     },
     { 
         "comment_store_id": 2,
         "user_name": "Michael Smith",
-        "user_pic": {},
+        "user_pic": "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=10155410113061073&height=200&width=200&ext=1529003986&hash=AeQa7dc2YcMAFiC4",
         "comment": "Neat spot! The store is small but jam packed with records. The staff is always helpful.",
         "rating": 4
     },
     { 
         "comment_store_id": 3,
         "user_name": "Michael Smith",
-        "user_pic": {},
+        "user_pic": "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=10155410113061073&height=200&width=200&ext=1529003986&hash=AeQa7dc2YcMAFiC4",
         "comment": "Selection is limited for records.",
         "rating": 3
     },
     { 
         "comment_store_id": 4,
         "user_name": "Michael Smith",
-        "user_pic": {},
+        "user_pic": "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=10155410113061073&height=200&width=200&ext=1529003986&hash=AeQa7dc2YcMAFiC4",
         "comment": "Very modern and clean looking store. Nice small selection.",
         "rating": 4
     },
     { 
         "comment_store_id": 4,
         "user_name": "John Williams",
-        "user_pic": {},
+        "user_pic": "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=10155410113061073&height=200&width=200&ext=1529003986&hash=AeQa7dc2YcMAFiC4",
         "comment": "Is an OK store. Not much to choose from.",
         "rating": 3
     }
