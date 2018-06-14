426a533c6eef3d6c2d719adb772c6559ed4ebf19
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-06-14 11:33:12 -0600

    correct user image address

--- seed_data_files/comments-seed-data.js
+++ seed_data_files/comments-seed-data.js
@@ -9,14 +9,14 @@ module.exports = [
     { 
         "comment_store_id": 1,
         "user_name": "Catherine Coffman",
-        "user_pic": "https://www.google.com/maps/contrib/102750362404937856411/reviews?hl=en-US&sa=X&ved=0ahUKEwiWgYSA09PbAhXm24MKHcYOCqIQwPQBCI0CMB4",
+        "user_pic": "https://lh5.googleusercontent.com/-PIOkDiQEtnM/AAAAAAAAAAI/AAAAAAAAAAA/tET4z7Z1pNQ/w120-h120-p-rp-mo-ba4-br100/photo.jpg",
         "comment": "The staff is helpful and the employees have a nice range of music knowledge.",
         "rating": 5
     },
     { 
         "comment_store_id": 1,
         "user_name": "Kailey Burns",
-        "user_pic": "https://www.google.com/maps/contrib/114022117176978183739/reviews?hl=en-US&sa=X&ved=2ahUKEwjjk-qG1NPbAhUFjq0KHRwWArAQvfQBegQIARAU",
+        "user_pic": "https://lh4.googleusercontent.com/-6Gn8Ec5Vx2E/AAAAAAAAAAI/AAAAAAAAAAA/689oFjbzxuo/w120-h120-p-rp-mo-ba4-br100/photo.jpg",
         "comment": "This place was pretty well stocked and had tons of different kinds of multimedia, including cassette tapes (WHAAAAT?!). The people working were very friendly and they played some really good tunes (score!). Over all would definitely return. Thanks for the sweet Nora Jones vinyl",
         "rating": 5
     },
@@ -31,14 +31,14 @@ module.exports = [
     { 
         "comment_store_id": 2,
         "user_name": "James Reynolds",
-        "user_pic": "https://www.google.com/maps/contrib/118041330318019763086/reviews?hl=en-US&sa=X&ved=2ahUKEwjKiIPB1NPbAhVFKawKHf4mDVYQvfQBegQIARBG",
+        "user_pic": "https://lh6.googleusercontent.com/-rT_NN9dCWDw/AAAAAAAAAAI/AAAAAAAAAAA/7iMiKjh1eNs/w120-h120-p-rp-mo-ba2-br100/photo.jpg",
         "comment": "I've lived within a block of Wax Trax for the past decade, and love this shop.  It's unique selection will always keep me coming back for more.",
         "rating": 5
     },
     { 
         "comment_store_id": 3,
         "user_name": "Jayme Bucy",
-        "user_pic": "https://www.google.com/maps/contrib/111343055379761146611/reviews?hl=en-US&sa=X&ved=2ahUKEwiI7-T11NPbAhUDSq0KHabyDsIQvfQBegQIARAI",
+        "user_pic": "https://lh4.googleusercontent.com/-_vwiPfplZw0/AAAAAAAAAAI/AAAAAAAAAAA/HqIcoBD8hOc/w120-h120-p-rp-mo-br100/photo.jpg",
         "comment": "Great little gem I found today while I was working. I ended up taking a longer break than needed but it was so worth it. They have an awesome selection of CD's and vinyl! They've got records for pennies so I will definitely be back so I can get some for my record wall! But very friendly staff, too!",
         "rating": 4
     },
@@ -52,7 +52,7 @@ module.exports = [
     { 
         "comment_store_id": 4,
         "user_name": "William Buchanan",
-        "user_pic": "https://www.google.com/maps/contrib/101702194025617117760/reviews?hl=en-US&sa=X&ved=2ahUKEwjwp-fc1dPbAhVDL6wKHVuFBwAQvfQBegQIARA-",
+        "user_pic": "https://lh5.googleusercontent.com/-MmSvG-yZhZU/AAAAAAAAAAI/AAAAAAAAAAA/uf9N-EwAziM/w120-h120-p-rp-mo-ba2-br100/photo.jpg",
         "comment": "Good experience! They support local artist. Checkout Sgt Remo newest Reggae Record.",
         "rating": 5
     },
